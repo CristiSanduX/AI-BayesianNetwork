124559213017898d197736c566abcaf6332cf80d
Cristi Sandu <cristi.sandu@csx.ro>
2024-01-04 15:19:51 +0200

    Add functionality to load Bayesian network from JSON file

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,43 +1,29 @@
+import json
 from pgmpy.models import BayesianModel
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
 
-# Crearea structurii rețelei bayesiene (arcele)
-model = BayesianModel([
-    ('Gripă', 'Febra'),
-    ('Abces', 'Febra'),
-    ('Febra', 'Oboseală'),
-    ('Febra', 'Anorexie')
-])
 
-###
-#   Tabelele de probabilități pentru rețeaua bayesiană
-###
+def load_model_from_json(json_file_path):
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
 
-# Definirea CPD-urilor pentru fiecare variabilă
-cpd_gripa = TabularCPD(variable='Gripă', variable_card=2, values=[[0.1], [0.9]])
-cpd_abces = TabularCPD(variable='Abces', variable_card=2, values=[[0.05], [0.95]])
-cpd_febra = TabularCPD(variable='Febra', variable_card=2,
-                       values=[[0.8, 0.7, 0.25, 0.05],
-                               [0.2, 0.3, 0.75, 0.95]],
-                       evidence=['Gripă', 'Abces'],
-                       evidence_card=[2, 2])
-cpd_oboseala = TabularCPD(variable='Oboseală', variable_card=2,
-                          values=[[0.6, 0.2],
-                                  [0.4, 0.8]],
-                          evidence=['Febra'],
-                          evidence_card=[2])
-cpd_anorexie = TabularCPD(variable='Anorexie', variable_card=2,
-                          values=[[0.5, 0.1],
-                                  [0.5, 0.9]],
-                          evidence=['Febra'],
-                          evidence_card=[2])
+        model = BayesianModel(data["edges"])
 
-# Asocierea CPD-urilor cu modelul
-model.add_cpds(cpd_gripa, cpd_abces, cpd_febra, cpd_oboseala, cpd_anorexie)
+        for node, cpd_data in data["cpds"].items():
+            cpd = TabularCPD(node, cpd_data["variable_card"], cpd_data["values"],
+                             evidence=cpd_data.get("evidence"),
+                             evidence_card=cpd_data.get("evidence_card"))
+            model.add_cpds(cpd)
 
-# Verificarea corectitudinii modelului
-assert model.check_model()
+        if not model.check_model():
+            raise ValueError("Modelul încărcat nu este valid.")
+
+        return model
+
+# Încărcarea modelului
+model_path = 'values.json'
+model = load_model_from_json(model_path)
 
 # Inițializarea motorului de inferență
 infer = VariableElimination(model)
@@ -46,3 +32,4 @@ infer = VariableElimination(model)
 # Probabilitatea ca o persoană să fie obosită dacă nu are gripă, nu are abces și nu are anorexie
 prob_oboseala = infer.query(variables=['Oboseală'], evidence={'Gripă': 1, 'Abces': 1, 'Anorexie': 1})
 print(prob_oboseala)
+
